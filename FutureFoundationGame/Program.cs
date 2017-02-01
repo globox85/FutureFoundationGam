9f854266b68bc456ab11277c01f60e15a44c8e94
Alexander Rehnman <globox85@gmail.com>
2017-02-01 20:18:13 +0100

    Very basic menu system implemented

--- FutureFoundationGame/Program.cs
+++ FutureFoundationGame/Program.cs
@@ -10,6 +10,30 @@ namespace FutureFoundationGame
     {
         static void Main(string[] args)
         {
+            MainMenu();
+        }
+
+        private static void MainMenu()
+        {
+            Console.WriteLine("DANGANRONPA: FUTURE FOUNDATION");
+            Console.WriteLine("1. New Game");
+            string menuChoice = Console.ReadLine();
+
+            if (menuChoice == "1")
+            {
+                GameSetup();
+            }
+        }
+
+        private static void GameSetup()
+        {
+            Console.Clear();
+            Console.Write("Two years ago, the world fell into despair when ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Junko Enoshima");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(" brainwashed the students of Hope's Peak Academy and spread her teachings over the world. To restore the world to its rightful state, the Future Foundation was formed.");
+            Console.ReadLine();
         }
     }
 }
